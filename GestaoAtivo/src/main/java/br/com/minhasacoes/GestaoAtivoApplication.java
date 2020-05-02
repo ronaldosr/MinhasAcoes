367eea362538b6a86dadc95da7628152d5b6be37
ronaldosr <ronaldosr@users.noreply.github.com>
2020-05-02 20:03:28 -0300

    Gestao de Ativos - Criacao da aplicacao

---
+++ GestaoAtivo/src/main/java/br/com/minhasacoes/GestaoAtivoApplication.java
@@ -0,0 +1,13 @@
+package br.com.minhasacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoAtivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoAtivoApplication.class, args);
+	}
+
+}
